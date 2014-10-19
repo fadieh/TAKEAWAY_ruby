a0e7d5d54fb0ffc1467ad103fa4fcb81b852589e
Fadie Hannona <fadiehannona@gmail.com>
2014-10-19 21:07:48 +0100

    food should initialise with a price

--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -5,8 +5,13 @@ describe Food do
 	context "when initialised" do
 
 		it "should have a name" do
-			burger = Food.new("Steak", 10.00)
-			expect(burger.name).to eq "Steak"
+			steak = Food.new("Steak", 10.00)
+			expect(steak.name).to eq "Steak"
+		end
+
+		it "should have a price" do
+			steak = Food.new("Steak", 10.00)
+			expect(steak.price).to eq 10.00
 		end
 	end
 end
