a1f2ae520ff3d1957b11582f1ac1a9c5415245c5
Fadie Hannona <fadiehannona@gmail.com>
2014-10-19 21:19:37 +0100

    Added methods for initialising various food

--- lib/food.rb
+++ lib/food.rb
@@ -7,4 +7,24 @@ class Food
 		@price = price
 	end	
 
+	def self.steak
+		new("Steak", 10.00)
+	end
+
+	def self.steaksandwich
+		new("Steak Sandwich", 7.00)
+	end
+
+	def self.buffaloburger
+		new("Buffalo Burger", 8.00)
+	end
+
+	def self.lager
+		new("Lager", 3.00)
+	end
+
+	def self.irn_bru
+		new("IRN BRU", 2.00)
+	end
+
 end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -13,5 +13,36 @@ describe Food do
 			steak = Food.new("Steak", 10.00)
 			expect(steak.price).to eq 10.00
 		end
+
+		it "steak initaliser" do
+			steak = Food.steak
+			expect(steak.name).to eq "Steak"
+			expect(steak.price).to eq 10.00
+		end
+
+		it "steak sandwich initialiser" do
+			steaksandwich = Food.steaksandwich
+			expect(steaksandwich.name).to eq "Steak Sandwich"
+			expect(steaksandwich.price).to eq 7.00
+		end
+
+		it "buffalo burger initialiser" do
+			buffalo = Food.buffaloburger
+			expect(buffalo.name).to eq "Buffalo Burger"
+			expect(buffalo.price).to eq 8.00
+		end
+
+		it "lager initialiser" do
+			lager = Food.lager
+			expect(lager.name).to eq "Lager"
+			expect(lager.price).to eq 3.00
+		end
+
+		it "irn bru initialiser" do
+			irnbru = Food.irn_bru
+			expect(irnbru.name).to eq "IRN BRU"
+			expect(irnbru.price).to eq 2.00
+		end
+
 	end
 end
